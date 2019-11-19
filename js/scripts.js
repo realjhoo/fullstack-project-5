a45775c6803be9c01d7529adc3a0bae0509d09e2
Jerry L Hoover <jerry.hoover@ymail.com>
2019-11-18 20:22:27 -0600

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -12,94 +12,35 @@ function getAPIdata() {
     })
     .then(employee_data => {
       for (let i = 0; i < numberOfEmployees; i++) {
-        // let employee_image = employee_data.results[i].picture.large;
-        // let employee_firstName = employee_data.results[i].name.first;
-        let employee_lastName = employee_data.results[i].name.last;
-        let employee_email = employee_data.results[i].email;
-        let employee_city = employee_data.results[i].location.city;
-        // data for modal card
-        let employee_cell = employee_data.results[i].cell;
-        let employee_street_number =
-          employee_data.results[i].location.street.number;
-        let employee_street = employee_data.results[i].location.street.name;
-
-        let employee_state = employee_data.results[i].location.state;
-        let employee_postalcode = employee_data.results[i].location.postcode;
-        // birthday delivers yyyy-mm-ddThh:mm:ss.xxxZ
-        let employee_birthday = employee_data.results[i].dob.date;
-        let employee_index = i;
-        /*
-        employee_birthday = parseBirthday(employee_birthday);
-          */
-        //log stuff
-
         // create employee data object to pass data
         let employeeData = [
           {
-            // image: employee_image,
             image: employee_data.results[i].picture.large,
-            // name: employee_firstName + " " + employee_lastName,
-            name: employee_data.results[i].name.first,
-
-            email: employee_email,
-            city: employee_city,
-            // address: employee_city + ", " + employee_state,
-            cell: employee_cell,
-            fullAddress:
-              employee_street_number +
+            name:
+              employee_data.results[i].name.first +
               " " +
-              employee_street +
+              employee_data.results[i].name.last,
+            email: employee_data.results[i].email,
+            city: employee_data.results[i].location.city,
+            // data for modal card
+            cell: employee_data.results[i].cell,
+            address:
+              employee_data.results[i].location.street.number +
               " " +
-              employee_city +
+              employee_data.results[i].location.street.name +
+              " " +
+              employee_data.results[i].location.city +
               ", " +
-              employee_state +
+              employee_data.results[i].location.state +
               " " +
-              employee_postalcode,
-            birthday: employee_birthday,
-            index: employee_index
+              employee_data.results[i].location.postcode,
+            birthday: formatBirthday(employee_data.results[i].dob.date),
+            index: i
           }
         ];
         showCard(employeeData);
       }
     });
-}
-
-// ========================================================
-function showCard(employeeData) {
-  const cardContainer = document.getElementById("gallery");
-
-  // card data
-  let employee_image = employeeData[0].image;
-  let employee_name = employeeData[0].name;
-  let employee_email = employeeData[0].email;
-  let employee_city = employeeData[0].city;
-  // let employee_address = employeeData[0].address;
-  // modal card data
-  let employee_cell = employeeData[0].cell;
-  let employee_full_address = employeeData[0].fullAddress;
-  let employee_birthday = employeeData[0].birthday;
-  let employee_index = employeeData[0].index;
-
-  // create the markup and drop in the variables
-  let cardMarkup = `<div class="card">
-                      <div class="card-img-container">
-                        <img class="card-img" src=${employee_image} alt="profile picture">
-                      </div>
-                      <div class="card-info-container">
-                        <h3 id="name" class="card-name cap">${employee_name}</h3>
-                        <p class="card-text card-email">${employee_email}</p>
-                        <p class="card-text card-city cap">${employee_city}</p>
-                      </div>
-                      <div class="card-data-container">
-                        <span class="card-cell">${employee_cell}</span>
-                        <span class="card-address">${employee_full_address}</span>
-                        <span class="card-birthday">${employee_birthday}</span>
-                        <span class="card-index">${employee_index}</span>
-                      </div>
-                  </div>`;
-
-  // insert in DOM
-  cardContainer.insertAdjacentHTML("beforeend", cardMarkup);
 }
 
 // ========================================================
@@ -110,12 +51,37 @@ function showSearchBox() {
                             <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
                         </form>`;
 
-  const searchContainer = document.getElementsByClassName("search-container");
-
-  searchContainer[0].innerHTML = searchMarkup;
+  const searchContainer = document.querySelector(".search-container");
+  searchContainer.innerHTML = searchMarkup;
 }
 
-// ************** MODAL ***********************************
+// ========================================================
+function showCard(employee) {
+  const gallery = document.getElementById("gallery");
+
+  // create the markup and drop in the variables
+  let cardMarkup = `<div class="card">
+                      <div class="card-img-container">
+                        <img class="card-img" src=${employee[0].image} alt="profile picture">
+                      </div>
+                      <div class="card-info-container">
+                        <h3 id="name" class="card-name cap">${employee[0].name}</h3>
+                        <p class="card-text card-email">${employee[0].email}</p>
+                        <p class="card-text card-city cap">${employee[0].city}</p>
+                      </div>
+                      <div class="card-data-container">
+                        <span class="card-cell">${employee[0].cell}</span>
+                        <span class="card-address">${employee[0].address}</span>
+                        <span class="card-birthday">${employee[0].birthday}</span>
+                        <span class="card-index">${employee[0].index}</span>
+                      </div>
+                  </div>`;
+
+  // insert in DOM
+  gallery.insertAdjacentHTML("beforeend", cardMarkup);
+}
+
+// ************** MODAL STUFF *****************************
 // ========================================================
 function createModalContainer() {
   const gallery = document.getElementById("gallery");
@@ -128,8 +94,8 @@ function createModalContainer() {
 // ========================================================
 function createModalCard(event) {
   const modalContainer = document.querySelector(".modal-container");
-  // retrieve data
 
+  // retrieve data
   let image = event.target.querySelector(".card-img").src,
     name = event.target.querySelector(".card-name").textContent,
     email = event.target.querySelector(".card-email").textContent,
@@ -153,6 +119,7 @@ function createModalCard(event) {
                  <p class="modal-text">${cell}</p>
                  <p class="modal-text">${address}</p>
                  <p class="modal-text">Birthday: ${birthday}</p>
+                 <p class="modal-index">${index}</p>
              </div>
          </div>
          <div class="modal-btn-container">
@@ -163,22 +130,22 @@ function createModalCard(event) {
   // place markup in DOM
   modalContainer.insertAdjacentHTML("beforeend", modalMarkup);
 
-  // activate close button listener
+  // activate modal listeners
+  modalPrevListener();
+  modalNextListener();
   modalCloseListener();
 
-  // show modal
+  // show modal box
   document.querySelector(".modal-container").style.display = "block";
 }
 
 // ************* EVENT LISTENERS **************************
 // ========================================================
-function activateCardListener() {
+function cardListener() {
   document.addEventListener("click", event => {
     if (event.target.className === "card") {
       createModalCard(event);
-    }
-
-    if (
+    } else if (
       event.target.className === "card-name cap" ||
       event.target.className === "card-img" ||
       event.target.className === "card-text card-email" ||
@@ -191,19 +158,110 @@ function activateCardListener() {
 
 // ========================================================
 function modalCloseListener() {
+  // event handler for modal clase
   const modalContainer = document.querySelector(".modal-container");
   document.querySelector(".modal-close-btn").addEventListener("click", () => {
     document.querySelector(".modal-container").style.display = "none";
+    // destroys modal card to prevent multiple modals in markup
     modalContainer.innerHTML = "";
   });
 }
 
 // ========================================================
+function modalPrevListener() {
+  // decrements to the previous card
+  const modalContainer = document.querySelector(".modal-container");
+
+  document.getElementById("modal-prev").addEventListener("click", () => {
+    let index = parseInt(document.querySelector(".modal-index").innerText);
+
+    if (index === 0) {
+      index = 11;
+    } else {
+      index--;
+    }
+
+    const cards = document.querySelectorAll(".card");
+    let card = cards[index];
+    modalContainer.innerHTML = "";
+    card.click();
+  });
+}
+
+// ========================================================
+function modalNextListener() {
+  // increments to the previous card
+  const modalContainer = document.querySelector(".modal-container");
+
+  document.getElementById("modal-next").addEventListener("click", () => {
+    let index = parseInt(document.querySelector(".modal-index").innerText);
+
+    if (index === 11) {
+      index = 0;
+    } else {
+      index++;
+    }
+
+    const cards = document.querySelectorAll(".card");
+    let card = cards[index];
+    modalContainer.innerHTML = "";
+    card.click();
+  });
+}
+
+// ========================================================
+function liveSearchListener() {
+  document.getElementById("search-input").addEventListener("keyup", () => {
+    const card = document.querySelectorAll(".card");
+    let searchString = document
+      .getElementById("search-input")
+      .value.toLowerCase();
+
+    for (let i = 0; i < card.length; i++) {
+      let name = card[
+        i
+      ].firstChild.nextSibling.nextSibling.nextSibling.firstChild.nextSibling.innerText.toLowerCase();
+
+      let isin = name.includes(searchString);
+      console.log(isin);
+
+      if (isin) {
+        card[i].style.display = "flex";
+        // card[i].style.backgroundColor = "red";
+      } else {
+        card[i].style.display = "none";
+        // card[i].style.backgroundColor = "blue";
+      }
+    }
+  });
+}
+
+// ********************* HELPER FUNCTIONS ******************
+// ========================================================
+function formatBirthday(birthday) {
+  // api delivers birthday in format yyyy-mm-ddThh:mm:ss.xxxZ
+  // reformat birthday
+  let fix = new Date(birthday),
+    month = fix.getMonth(),
+    date = fix.getDate(),
+    year = fix.getYear();
+
+  if (month === 0) {
+    month++;
+  }
+
+  let fixedBirthday = month + "/" + date + "/" + year;
+  return fixedBirthday;
+}
+
+// ********************** MAIN ****************************
+// ========================================================
 function main() {
-  showSearchBox();
   getAPIdata();
+  showSearchBox();
   createModalContainer();
-  activateCardListener();
+  cardListener();
+  liveSearchListener();
 }
 
 // --------------------------------------------------------
